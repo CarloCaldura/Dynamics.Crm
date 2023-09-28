9ace782dcd0023f216e750133969c2cc99d397f1
Carlo Caldura <carlo.caldura@gmail.com>
2023-09-28 15:13:52 +0200

    Update boutique.js plugin

--- Dynamics.Crm.Resources/src/entities/boutique.js
+++ Dynamics.Crm.Resources/src/entities/boutique.js
@@ -7,6 +7,8 @@ formlogic = {
         //let nameAttribute = getAttribute("ald_name");	//Table is Boutique  (controlla se va altrimenti formContext.getAttribute("ald_name"))
         this.makeAddressDefaultValue();
         this.makeAddressReadOnly();
+        formContext.getAttribute("statuscode").addOnChange(this.statusReasonReadOnly);
+        this.statusReasonReadOnly();
     },
 
     makeAddressReadOnly: function () {
@@ -17,7 +19,28 @@ formlogic = {
         if (formContext.ui.getFormType() === 1) {
             formContext.getAttribute("ald_address").setValue("Via Fontane 93"); //getAttribute mi permette di settare o fare update del valore di un campo
         }
-        
+    },
+
+    // when status reason is read only tutti gli altri campi vanno in read only
+    statusReasonReadOnly: function () {
+        let statusCodeValue = formContext.getAttribute("statuscode").getValue();
+        if (statusCodeValue === 748890001) {
+            formContext.ui.controls.forEach(
+                (control) => {
+                    if (control.getName() !== "statuscode") {
+                        control.setDisabled(true);
+                    }
+                }
+            );
+        } else {
+            formContext.ui.controls.forEach(
+                (control) => {
+                    if (control.getName() !== "ald_address") {
+                        control.setDisabled(false);
+                    }
+                }
+            );
+        }
     },
 
     command: function (executionContext) {
